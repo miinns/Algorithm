454573be675d31b10b72a9515bf866ebe8939c84
miinns <113820366+miinns@users.noreply.github.com>
2025-05-18 02:12:40 +0900

    [level 2] Title: 타겟 넘버, Time: 5.92 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,19 +1,26 @@
 class Solution {
-    int answer = 0;
-
-    public int solution(int[] numbers, int target) {
-        dfs(numbers, 0, target, 0);
-        
-        return answer;
-    }
+    int[] numbers;
+    int target;
+    int answer;
     
-    // DFS
-    public void dfs(int[] numbers, int depth, int target, int sum){
-        if(depth == numbers.length) { 
-            if(target == sum) answer++;
-        } else {
-            dfs(numbers, depth + 1, target, sum + numbers[depth]); 
-            dfs(numbers, depth + 1, target, sum - numbers[depth]); 
+    // dfs
+    void dfs(int index, int sum) {
+        // 1. 재귀 탈출 조건
+        if(index == numbers.length) {
+            if(sum == target) answer++;
+            return;
         }
+        // 2. 수행 동작
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
+    }
+    public int solution(int[] numbers, int target) {
+        answer = 0;
+        this.numbers = numbers;
+        this.target = target;
+        
+        // dfs (0,0) 호출 (첫 번째 파라미터 index, 두 번째 파라미터 sum)
+        dfs(0, 0);
+        return answer;
     }
 }
